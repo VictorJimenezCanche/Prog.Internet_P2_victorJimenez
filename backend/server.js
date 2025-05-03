e78046d08b5f96341955825be7f3d81802f65d5d
Victor Jimenez <v.jimenezcanche@gmail.com>
2025-05-02 18:01:25 -0600

    arreglando start

--- backend/server.js
+++ backend/server.js
@@ -5,20 +5,31 @@ const dotenv = require('dotenv').config()
 const connectDB = require('./config/db')
 const { errorHandler } = require('./middleware/errorMiddleware')
 const { notFound } = require('./middleware/notFoundMiddleware')
-const port = process.env.PORT || 5000
+
+const app = express()
+const PORT = process.env.PORT || 5000
 
 
 connectDB()
 
-const app = express()
 
 app.use(cors())
 app.use(express.json())
-app.use(express.urlencoded({extended: false}))
-app.use(errorHandler)
-app.use(notFound)
+app.use(express.urlencoded({ extended: false }))
+
+
+app.get('/', (req, res) => {
+    res.send('API funcionando correctamente')
+})
+
 
 app.use('/api/donaciones', require('./routes/donacionesRoutes'))
 app.use('/api/cards', require('./routes/fotosCardMilanesoRoutes'))
 
-app.listen(port, ()=> console.log(`Servidor iniciado en el puerto ${port}`))
+
+app.use(notFound)
+app.use(errorHandler)
+
+app.listen(PORT, () => {
+    console.log(`Servidor iniciado en el puerto ${PORT}`)
+})
